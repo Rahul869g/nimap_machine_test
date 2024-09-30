ecd5549206f8c19565e75efb47468600e5362320
Rahul Gupta <rahul123alok@gmail.com>
2024-09-30 11:25:21 +0530

    added env

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // API key for The Movie Database (TMDb)
-export const API_KEY = "c45a857c193f6302f2b5061c3b85e743";
+export const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 // Base URL for TMDb API
 export const TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
